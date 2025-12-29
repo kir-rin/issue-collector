a251080a5c206ed9b101cff9788c9fd351fb7d0f
dst03106 <dstdbsgml@gmail.com>
2025-12-29 17:05:11 +0900

    Revert "chore: upload artifact on release branch push for lambda"
    This reverts commit 9f2b7bea5f8c87db7e4bc7ec6afd165cf0848305.

--- scripts/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sh /app/scripts/import-workflow.sh
-WORKFLOW_ID=$(sqlite3 ~/.n8n/database.sqlite "SELECT id FROM workflow_entity ORDER BY createdAt DESC LIMIT 1;")
-npx n8n execute workflow --id $WORKFLOW_ID
