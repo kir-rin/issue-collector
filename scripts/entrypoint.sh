3b63cc41bb90d7e6b73fb07998e2cc7dab3961fc
dst03106 <dstdbsgml@gmail.com>
2026-01-04 17:15:06 +0900

    feat: remove npm wrapper for workflow import to fix stdout buffering

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-set -e
-npx n8n-import --loglevel=verbose --logs-max=0 
+node scripts/import-workflow.js
 WORKFLOW_ID=$(sqlite3 $N8N_USER_FOLDER/.n8n/database.sqlite "SELECT id FROM workflow_entity ORDER BY createdAt DESC LIMIT 1;")
 npx n8n execute workflow --id $WORKFLOW_ID --loglevel=verbose --logs-max=0
