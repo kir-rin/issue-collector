1de5eda61a7f5b28cc9eae8e313629c3c8d48127
dst03106 <dstdbsgml@gmail.com>
2026-02-08 00:10:24 +0900

    refactor: add FROM_EMAIL env variable support for sender configuration

--- resources/send-email.js
+++ resources/send-email.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "fromEmail": process.env.EMAIL,
+    "fromEmail": process.env.FROM_EMAIL || process.env.EMAIL,
     "toEmail": process.env.EMAIL,
 		"subject": "=[Issue Report] {{ $('Load Repo Info').first().json.owner }}/{{ $('Load Repo Info').first().json.name }} - {{ $('Title Generator Langchain Agent').first().json.title }}",
     "html": "={{ $('convert MJML to HTML').item.json.htmlOutput.html }}",
