681344bd3840a0e989a72bcb83d7754b90bfb75b
kir-rin <dstdbsgml@gmail.com>
2026-05-17 12:13:22 +0900

    feat: add email display name and remove duplicate [Issue Report] prefix

--- resources/send-email.js
+++ resources/send-email.js
@@ -1,7 +1,7 @@
 module.exports = {
     "fromEmail": process.env.EMAIL,
     "toEmail": process.env.EMAIL,
-		"subject": "=[Issue Report] {{ $('Load Repo Info').first().json.owner }}/{{ $('Load Repo Info').first().json.name }} - {{ $('Title Generator Langchain Agent').first().json.title }}",
+		"subject": "=[{{ $('Load Repo Info').first().json.owner }}/{{ $('Load Repo Info').first().json.name }}] {{ $('Title Generator Langchain Agent').first().json.title }}",
     "html": "={{ $('convert MJML to HTML').item.json.htmlOutput.html }}",
     "options": {}
 };
